b1e620d5c7dfd70d01b206b943a1a4d0d03c77b4
Jared Hiller <hillerj@wit.edu>
2023-04-04 10:49:24 -0400

    added build functions

--- Main.c
+++ Main.c
@@ -38,12 +38,26 @@ int main() {
     int left_y2 = left_y1 + 50 - 1;
 
 
+    int ball_midpoint, left_midpoint, right_midpoint;
+
+    int ball_inc_y;
+
+    int check = 0;
+
+    ball_midpoint = (bally1 + bally2) / 2;  //finds the middle of the ball in the y-direction
+
+    left_midpoint = (left_y1 + left_y2) / 2;
+
 
 
 
     //y inc, change in the y parameter per frame
-    int ball_inc = 0;
+    int ball_y_inc = 0;
     int ball_x_inc = 0;
+
+    VGAbuild(ballx1, bally1, ballx2, bally2, 0x808080);
+    VGAbuild(right_x1, right_y1, right_x2, right_y2, 0x808080);
+    VGAbuild(left_x1, left_y1, left_x2, left_y2, 0x808080);
 
 
     while (1) {
@@ -56,20 +70,181 @@ int main() {
             ball_x_speed = -ball_x_speed;
             ball_x_inc = -ball_x_inc;
 
-            ball_x_speed = ball_x_inc;
-            ball_x_inc++;
+            ball_x_speed += ball_x_inc;
+         
         }
-
         //hits left paddle, switch x direction, and adds to the x direction speed
         else if (ballx2 == 20 && ((bally1 > left_y1) || (bally2 < left_y2))) {
             ball_x_speed = -ball_x_speed;
             ball_x_inc = -ball_x_inc;
 
-            ball_x_speed = ball_x_inc;
-            ball_x_inc++;
+            ball_x_speed += ball_x_inc;
+            
+        }
+        //if it hits the top or bottom of the screen
+        else if (bally1 == 229 || bally2 == 11) {
+            ball_y_inc = -ball_y_inc;
+        }
+        if ((ballx1 == left_x2) && (bally1 >= left_y1) && (bally2 <= left_y2)) //if ball hits left paddle
+
+        {
+
+            if ((ball_midpoint >= left_midpoint - 1) && (ball_midpoint <= left_midpoint + 2))
+
+            {
+
+                ball_inc_y = 0;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= left_midpoint - 4) && (ball_midpoint <= left_midpoint - 2) && (check != 1))
+
+            {
+
+                ball_inc_y = -1;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= left_midpoint + 3) && (ball_midpoint <= left_midpoint + 5) && (check != 1))
+
+            {
+
+                ball_inc_y = 1;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= left_midpoint - 14) && (ball_midpoint <= left_midpoint - 5) && (check != 1))
+
+            {
+
+                ball_inc_y = -2;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= left_midpoint + 6) && (ball_midpoint <= left_midpoint + 15) && (check != 1))
+
+            {
+
+                ball_inc_y = 2;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= left_midpoint - 24) && (ball_midpoint <= left_midpoint - 15) && (check != 1))
+
+            {
+
+                ball_inc_y = -3;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= left_midpoint + 16) && (ball_midpoint <= left_midpoint + 25) && (check != 1))
+
+            {
+
+                ball_inc_y = 3;
+
+                check = 1;
+
+            }
+
+            check = 0;
+
         }
 
+        if ((ballx2 == right_x1) && (bally1 >= right_y1) && (bally2 <= right_y2)) //if ball hits right paddle
 
+        {
+
+            if ((ball_midpoint >= right_midpoint - 1) && (ball_midpoint <= right_midpoint + 2))
+
+            {
+
+                ball_inc_y = 0;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= right_midpoint - 4) && (ball_midpoint <= right_midpoint - 2) && (check != 1))
+
+            {
+
+                ball_inc_y = -1;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= right_midpoint + 3) && (ball_midpoint <= right_midpoint + 5) && (check != 1))
+
+            {
+
+                ball_inc_y = 1;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= right_midpoint - 14) && (ball_midpoint <= right_midpoint - 5) && (check != 1))
+
+            {
+
+                ball_inc_y = -2;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= right_midpoint + 6) && (ball_midpoint <= right_midpoint + 15) && (check != 1))
+
+            {
+
+                ball_inc_y = 2;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= right_midpoint - 24) && (ball_midpoint <= right_midpoint - 15) && (check != 1))
+
+            {
+
+                ball_inc_y = -3;
+
+                check = 1;
+
+            }
+
+            if ((ball_midpoint >= right_midpoint + 16) && (ball_midpoint <= right_midpoint + 25) && (check != 1))
+
+            {
+
+                ball_inc_y = 3;
+
+                check = 1;
+
+            }
+
+            check = 0;
+
+        }
+
+        clearScreen();
+        VGAbuild(ballx1, bally1, ballx2, bally2, 0x808080);
+        VGAbuild(right_x1, right_y1, right_x2, right_y2, 0x808080);
+        VGAbuild(left_x1, left_y1, left_x2, left_y2, 0x808080);
 
 
 
